4ec9cbf2daae6f3f9a3b5ad57be67634397fd97d
Leslie Richardson <lerich@microsoft.com>
2024-05-04 12:26:26 -0700

    Fixed startup bug

--- src/ReadingList/Startup.cs
+++ src/ReadingList/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using ReadingList.Models;
+using Microsoft.Extensions.Hosting;
 
 namespace ReadingList
 {
